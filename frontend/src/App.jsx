38221a80ea7596ac2ef72b1e88ba74e9508f8900
Vuggam Manoj <vuggammanoj@gmail.com>
2024-09-15 14:13:35 +0530

    First Commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,21 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Navbar from './components/Navbar/Navbar'
 import { Route, Routes } from 'react-router-dom'
 
 import Cart from './pages/Cart/Cart'
 import Home from './pages/Home/Home'
 import PlaceOrder from './pages/PlaceOrder/PlaceOrder'
+import Footer from './components/Navbar/Footer/Footer'
+import LoginPopup from './components/Navbar/LoginPopup/LoginPopup'
 
 const App = () => {
+  const [showLogin,setShowLogin]=useState(false)
   return (
+    <>
+    {showLogin?<LoginPopup setShowLogin={setShowLogin}/>:<></>}
     <div className='app'>
-      <Navbar/>
+      <Navbar setShowLogin={setShowLogin}/>
       <Routes>
         <Route path='/' element={<Home/>}/>
         <Route path='/cart' element={<Cart/>}/>
         <Route path='/order' element={<PlaceOrder/>}/>
+
       </Routes>
     </div>
+    <Footer/>
+    </>
   )
 }
 
--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -3,6 +3,7 @@ import './Home.css'
 import Header from '../../components/Navbar/Header/Header'
 import ExploreMenu from '../../components/Navbar/ExploreMenu/ExploreMenu'
 import FoodDisplay from '../../components/Navbar/FoodDisplay/FoodDisplay'
+import AppDownload from '../../components/Navbar/AppDownload/AppDownload'
 const Home = () => {
     const [category,setCategory]=useState("All");
   return (
@@ -10,6 +11,7 @@ const Home = () => {
       <Header/>
       <ExploreMenu category={category} setCategory={setCategory}/>
       <FoodDisplay category={category}/>
+      <AppDownload/>
     </div>
   )
 }
